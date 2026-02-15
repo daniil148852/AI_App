ca5bb0ecd973c6d77531986ae6c3c0e97e2e6cd1
daniil148852 <daniildan850@gmail.com>
2026-02-15 09:59:12 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-pluginManagement {
+dependencyResolution {
     repositories {
         google()
         mavenCentral()
@@ -6,7 +6,7 @@ pluginManagement {
     }
 }
 
-dependencyResolution {
+dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
@@ -16,3 +16,4 @@ dependencyResolution {
 
 rootProject.name = "AI-Phone-Assistant"
 include(":app")
+
